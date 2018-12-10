10cd75ef20253d9ae5c7f5259109ca81c2ac2a47
Appropriation <35183519+Appropriation@users.noreply.github.com>
2018-12-10 07:05:21 -0600

    Update Anime.js

--- Anime.js
+++ Anime.js
@@ -6,29 +6,29 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Log#') {
-    	message.reply('Current Log 2016-2017 Last log before shutdown');
+    if (message.content === 'Goodnight') {
+    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiJ8NnqqJXfAhVByYMKHfTYAuUQjRx6BAgBEAU&url=https%3A%2F%2Fgifimage.net%2Fanime-sleep-gif-7%2F&psig=AOvVaw2kDHP8sW2QAmqIupIYsicT&ust=1544533222027407');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'No u') {
-    	message.reply('You have achieved the big homosapian, miss me with that gay shit');
+    if (message.content === 'Goodmorning') {
+    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi0yOWUqZXfAhWHq4MKHYbZD2QQjRx6BAgBEAU&url=http%3A%2F%2Fpokemonfanon.wikia.com%2Fwiki%2FFile%3ANodoka_waking_up.gif&psig=AOvVaw22GGGIPh_bWJMEXJgR_7rf&ust=1544533296885345');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'no u') {
-    	message.reply('Good commies do not use capitals because they are not capitalist scumbags good work commrades *stands and solutes*');
+    if (message.content === 'Adorable') {
+    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwifmt69qZXfAhUl44MKHVBEAq4QjRx6BAgBEAU&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fcute-little-anime-girl-gifs&psig=AOvVaw2ds3Z8Akso6B8vScveY4gZ&ust=1544533390183705');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'Shreck Meme') {
-    	message.reply('https://cdn.discordapp.com/attachments/380292099377201162/515270334174461952/tumblr_pie02giID21xoyw8po1_400.gif');
+    if (message.content === 'Spice') {
+    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiTtrHXqZXfAhXK3YMKHco6CD8QjRx6BAgBEAU&url=https%3A%2F%2Fsteamcommunity.com%2Fsharedfiles%2Ffiledetails%2F%3Fid%3D846908828&psig=AOvVaw2-0a2Vlyg8sJwe46_jbqWx&ust=1544533447797652');
   	}
 });
                                                                                                                                                                                                                                                         
                                                                                                                               
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTIxNjY4MjExNDM2MDI3OTA0.Du_1_Q.Hx7BfblvQnFQ2DARcAfeQKadmOc);
