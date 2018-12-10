98d67100b25f190a3e9340356198decd331d72e9
Appropriation <35183519+Appropriation@users.noreply.github.com>
2018-12-10 07:41:02 -0600

    Update Anime.js

--- Anime.js
+++ Anime.js
@@ -7,25 +7,25 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'Goodnight') {
-    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiJ8NnqqJXfAhVByYMKHfTYAuUQjRx6BAgBEAU&url=https%3A%2F%2Fgifimage.net%2Fanime-sleep-gif-7%2F&psig=AOvVaw2kDHP8sW2QAmqIupIYsicT&ust=1544533222027407');
+    	message.reply('https://gifimage.net/wp-content/uploads/2017/09/anime-sleep-gif-7.gif');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Goodmorning') {
-    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi0yOWUqZXfAhWHq4MKHYbZD2QQjRx6BAgBEAU&url=http%3A%2F%2Fpokemonfanon.wikia.com%2Fwiki%2FFile%3ANodoka_waking_up.gif&psig=AOvVaw22GGGIPh_bWJMEXJgR_7rf&ust=1544533296885345');
+    	message.reply('https://vignette.wikia.nocookie.net/pokemonfanon/images/6/63/Nodoka_waking_up.gif/revision/latest?cb=20161110064638');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Adorable') {
-    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwifmt69qZXfAhUl44MKHVBEAq4QjRx6BAgBEAU&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fcute-little-anime-girl-gifs&psig=AOvVaw2ds3Z8Akso6B8vScveY4gZ&ust=1544533390183705');
+    	message.reply('https://media1.tenor.com/images/86aece98ba2859b552825808b48cfbd9/tenor.gif?itemid=5948544');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'Spice') {
-    	message.reply('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiTtrHXqZXfAhXK3YMKHco6CD8QjRx6BAgBEAU&url=https%3A%2F%2Fsteamcommunity.com%2Fsharedfiles%2Ffiledetails%2F%3Fid%3D846908828&psig=AOvVaw2-0a2Vlyg8sJwe46_jbqWx&ust=1544533447797652');
+    	message.reply('https://steamusercontent-a.akamaihd.net/ugc/163654259564205421/86463FD8B6EB83F487FC7829393A3774CD80A51F/');
   	}
 });
                                                                                                                                                                                                                                                         
